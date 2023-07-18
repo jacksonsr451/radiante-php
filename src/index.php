3fd6efa5f1fd6d4685d76fdbef9bd5fb1a58a159
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2023-07-18 18:31:14 -0300

    feat: init index.php

--- src/index.php
+++ src/index.php
@@ -1,1 +1,9 @@
 <?php
+
+use Jacksonsr45\RadiantPHP\Http\Request;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+require_once __DIR__ . '/../src/routes.php';
+
+$router->handleRequest(new Request($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']));
