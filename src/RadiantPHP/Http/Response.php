f378adbefd2c7cd6249e452f2b91accb9fabb4fe
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2023-07-18 18:22:14 -0300

    feat: init Response.php

---
+++ src/RadiantPHP/Http/Response.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Jacksonsr45\RadiantPHP\Http;
+
+class Response
+{
+    private $statusCode;
+    private $headers;
+    private $body;
+
+    public function __construct($statusCode = 200)
+    {
+        $this->statusCode = $statusCode;
+        $this->headers = [];
+        $this->body = '';
+    }
+
+    public function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+    }
+
+    public function setHeader($name, $value)
+    {
+        $this->headers[$name] = $value;
+    }
+
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+
+    public function send()
+    {
+        http_response_code($this->statusCode);
+        foreach ($this->headers as $name => $value) {
+            header("$name: $value");
+        }
+
+        echo $this->body;
+    }
+
+    public function sendJson($data)
+    {
+        $this->setHeader('Content-Type', 'application/json');
+        $json = json_encode($data, JSON_UNESCAPED_UNICODE);
+
+        $this->setBody($json);
+        $this->send();
+    }
+}
